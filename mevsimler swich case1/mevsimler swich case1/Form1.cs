832d3d155b4e851927f4c8fdc67ac25f6d0db155
MehmetIslamDag <dagislam4734@gmail.com>
2025-01-10 01:12:10 +0300

    Add files via upload

---
+++ mevsimler swich case1/mevsimler swich case1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mevsimler_swich_case1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int mevsim = Convert.ToInt32(textBox1.Text);
+            switch (mevsim)
+            {
+                case 1:
+                    this.BackColor = Color.White;
+                    MessageBox.Show("Temanız kış olmuştur");
+                    break;
+                case 2:
+                    this.BackColor = Color.DarkOrange;
+                    MessageBox.Show("Temanız Sonbahar Olmuştur");
+                    break;
+                case 3:
+                    this.BackColor = Color.Yellow;
+                    MessageBox.Show("Temanız Yaz Olmuştur");
+                    break;
+                case 4:
+                    this.BackColor = Color.Coral;
+                    MessageBox.Show("Temanız İlkbahar Olmuştur");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
