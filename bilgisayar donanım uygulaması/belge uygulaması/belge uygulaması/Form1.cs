948c8c989816ab3aec9a64d5d6e51eb1a73c80db
MehmetIslamDag <dagislam4734@gmail.com>
2025-01-10 00:56:19 +0300

    Add files via upload

---
+++ bilgisayar donanım uygulaması/belge uygulaması/belge uygulaması/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace belge_uygulaması
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int ortalama;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ortalama = Convert.ToInt32(textBox1.Text);
+            if (ortalama >=0 && ortalama <50)
+            {
+                MessageBox.Show("Öğrenci Sınıfta kalmıştır");
+            }
+            if (ortalama >49 && ortalama <70)
+            {
+                MessageBox.Show("Öğrenci Belgesiz Geçmiştir");
+            }
+            if (ortalama >69 && ortalama <85)
+            {
+                MessageBox.Show("Öğrenci Teşekkür Belgesi İle Geçmiştir");
+            }
+            if (ortalama >84 && ortalama < 101)
+            {
+                MessageBox.Show("Öğrenci Takdir Belgesi İle Geçmiştir");
+            }
+        }
+    }
+}
---
+++ bilgisayar donanım uygulaması/bilgisayar donanım uygulaması/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bilgisayar_donanım_uygulaması
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string secim;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            secim = listBox1.SelectedItem.ToString();
+            if (secim=="MOUSE"||secim == "KLAVYE"||secim == "KAMERA"||secim == "TARAYICI")
+            {
+                MessageBox.Show("bu parça,giriş aygıtıdır");
+            }
+            if (secim == "YAZICI" || secim == "HOPERLÖR" || secim == "PROJEKSİYON")
+            {
+                MessageBox.Show("bu parça,çıkış aygıtıdır");
+            }
+        }
+    }
+}
