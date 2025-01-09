dc6d01c925105210cc5104493a571815afbe5178
MehmetIslamDag <dagislam4734@gmail.com>
2025-01-10 01:09:40 +0300

    Add files via upload

---
+++ Mac Skoru/Mac Skoru/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mac_Skoru
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int skor1, skor2;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            skor1 = Convert.ToInt32(textBox1.Text);
+            skor2 = Convert.ToInt32(textBox2.Text);
+            if (skor1 > skor2)
+            {
+                MessageBox.Show(" Beşiktaş Kazandı");
+            }
+            if (skor1 < skor2)
+            {
+                MessageBox.Show("Real Madrid Kazandı ");
+            }
+            else
+            {
+                MessageBox.Show("Maç Berabere Bitmiştir");
+            }
+        }
+    }
+}
---
+++ kullanici girişi/kullanici girişi/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kullanici_girişi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string kullanici_adi, sifre;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            kullanici_adi=textBox1.Text;
+            sifre = textBox2.Text;
+            if(kullanici_adi=="islam47" && sifre=="4734")
+            {
+                MessageBox.Show("GİRİŞ BAŞARILI");
+            }
+            else
+            {
+                MessageBox.Show("kullanıcı adınız veya adınız yanlış girilmiştir");
+            }
+
+        }
+    }
+}
