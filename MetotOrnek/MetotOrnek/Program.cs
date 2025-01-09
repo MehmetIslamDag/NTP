dbcbb027bdfd0d2ca7516b4c2935e8ac88feb15d
MehmetIslamDag <dagislam4734@gmail.com>
2025-01-10 01:10:39 +0300

    Add files via upload

---
+++ MetotOrnek/MetotOrnek/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetotOrnek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BuyuklSayi buyuk = new BuyuklSayi();
+            int a = 250;
+            int b = 50;
+            int sonuc = buyuk.BuyukSayiyiBul(a, b);
+            Console.WriteLine("büyük olan sayı : "+ sonuc);
+            Console.ReadLine();
+        }
+    }
+}
